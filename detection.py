7c640a73dc01029f16b2f73001a5e2ce582a9de9
Shawn Im <shawnim8@gmail.com>
2018-04-03 00:41:18 -0400

    created obj detection code

--- detection.py
+++ detection.py
@@ -16,14 +16,14 @@ args = vars(ap.parse_args())
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle",
     "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike",
     "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
-COLORS = [np.random.uniform(0, 255, size = (len(CLASSES), 3))]
+COLORS = np.random.uniform(0, 255, size = (len(CLASSES), 3))
 
 print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
 image = cv2.imread(args["image"])
 (h, w) = image.shape[:2]
-blab = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843,
+blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843,
     (300, 300), 127.5)
 
 print("[INFO] computing object detections...")
@@ -41,10 +41,14 @@ for i in np.arange(0, detections.shape[2]):
         label = "{}: {:.2f}%".format(CLASSES[idx], confidence*100)
         print("[INFO] {}".format(label))
         cv2.rectangle(image, (startX, startY), (endX, endY),
-            COLORS[idx], 2)
+            COLORS[idx], 4)
         y = startY - 15 if startY - 15 > 15 else startY + 15
         cv2.putText(image, label, (startX, y), 
-            cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx] = 2)
+            cv2.FONT_HERSHEY_SIMPLEX, 3, COLORS[idx], 3)
 
-cv2.imshow("Output", image)
+width = int(image.shape[0])
+height = int(image.shape[1])
+ratio = width/height
+imageDisplay = cv2.resize(image, (500, int(500*ratio)))
+cv2.imshow("Output", imageDisplay)
 cv2.waitKey(0)
